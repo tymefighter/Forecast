2ab26913ee7e863ab6d3cec96fc7dd248615d641
Ahmed Zaheer Dadarkar <ahmed.dadarkar@gmail.com>
2020-10-20 01:06:07 +0530

    Add predict functions

--- Model1/predict.py
+++ Model1/predict.py
@@ -6,3 +6,43 @@ def computeAttentionWeights(
         self.S,
         state
     )))
+
+def predictOneTimestep(
+    self,
+    gruState,
+    X, 
+    currTime
+):
+    nextState, _ = self.gru(np.expand_dims(X[i], 0), gruState)
+
+    attentionWeights = self.computeAttentionWeights(nextState)
+    extremePred = tf.math.reduce_sum(
+        attentionWeights * self.q, 
+        axis = 0
+    )
+
+    if Y[i] > self.epsilon:
+        extremeTarget = 1
+        numExtremeEvents += 1
+    else:
+        extremeTarget = 0
+        numNormalEvents += 1
+
+    yPred = semiPred + self.b * extremePred
+    return yPred, nextState
+
+def predictOutput(self, X):
+
+    n = X.shape[0]
+    state = self.gru.get_initial_state()
+    Y = [None] * n
+
+    for i in range(n):
+        Y[i], state = self.predictOneTimestep(
+            state,
+            X,
+            i
+        )
+
+    Y = np.array(Y)
+    return Y
