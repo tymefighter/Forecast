83ccd273fde3ed6ea12e690ad7ae8b54ed71ce25
Ahmed Zaheer Dadarkar <ahmed.dadarkar@gmail.com>
2020-10-20 00:32:56 +0530

    Add required losses

--- Model1/loss.py
+++ Model1/loss.py
@@ -1,11 +1,57 @@
+import tensorflow as tf
+
 def squareLoss(yPred, yTrue):
-    pass
+    return tf.math.square(yPred - yTrue)
 
-def extremeValueLoss(pred, target):
-    pass
+def extremeValueLoss(
+    pred, 
+    target,
+    numNormalEvents,
+    numExtremeEvents
+):
+    extremePart = - numNormalEvents \
+        * tf.math.pow(
+            1 - pred / self.extremeValueIndex, 
+            self.extremeValueIndex
+        ) \
+        * target \
+        * tf.math.log(pred)
 
-def loss1(yPred, yTrue, extremePred, extremeTarget):
-    pass
+    normalPart = - numNormalEvents \
+        * tf.math.pow(
+            1 - (1 - pred) / self.extremeValueIndex, 
+            self.extremeValueIndex
+        ) \
+        * (1 - target) \
+        * tf.math.log(1 - pred)
 
-def loss2(preds, targets):
-    pass
+    return extremePart + normalPart
+
+def loss1(
+    yPred, 
+    yTrue, 
+    extremePred, 
+    extremeTarget,
+    extremeWeight,
+    numNormalEvents,
+    numExtremeEvents
+):
+    return squareLoss(yPred, yTrue) + \
+            extremeWeight * \
+            extremeValueLoss(
+                extremePred, 
+                extremeTarget,
+                numNormalEvents,
+                numExtremeEvents
+            )
+
+def loss2(
+    preds, 
+    targets,
+    numNormalEvents,
+    numExtremeEvents
+):
+    tf.reduce_sum(extremeValueLoss(
+        preds,
+        targets
+    ), axis = 0)
