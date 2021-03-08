c1efa98fa913a361fd80448bdc6fa512a2d036b5
Ahmed Zaheer Dadarkar <ahmed.dadarkar@gmail.com>
2021-03-08 17:37:24 +0530

    Add doc

--- ts/experimental/pso.py
+++ ts/experimental/pso.py
@@ -9,10 +9,19 @@ class Pso:
             numParticles
     ):
         """
+        Compute initial positions of particles by sampling for each
+        component of each particle from the uniform distribution
+        with end points specified for each component as an argument
+        taken by this function.
 
-        :param paramRange:
-        :param numParticles:
-        :return:
+        :param paramRange: list of 2-tuples (low, high) of length equal
+        to the dimension of the parameter space. There should be a
+        tuple for every dimension. Hence, for dimension d, the tuple
+        (low, high) says that each particle position's dth component must
+        be sampled from uniform(low, high).
+        :param numParticles: number of particles
+        :return: initial position matrix of shape (numParticles, dimension)
+        containing initial position vectors for each particle.
         """
 
         dim = len(paramRange)
@@ -38,8 +47,8 @@ class Pso:
         Particle Swarm Optimization algorithm
 
         :param minFunc: function which is to be minimized. It must
-        accept a numpy array of shape (dim,) where dim is the
-        dimension of the parameter space
+        accept a numpy array of shape (dim,) where dim is the dimension
+        of the parameter space
         :param initialPos: initial positions for each of the particles.
         It should be a numpy array of shape (numParticles, dim)
         :param inertiaCoeff: coefficient used for updating the velocity
@@ -108,14 +117,27 @@ class Pso:
             numIterations=20
     ):
         """
+        PSO algorithm with the addition of constriction coefficients.
+        This eliminates the use of damping for the inertia coefficient.
 
-        :param minFunc:
-        :param initialPos:
-        :param kappa:
-        :param phi1:
-        :param phi2:
-        :param numIterations:
-        :return:
+        :param minFunc: function which is to be minimized. It must
+        accept a numpy array of shape (dim,) where dim is the dimension
+        of the parameter space
+        :param initialPos: initial positions for each of the particles.
+        It should be a numpy array of shape (numParticles, dim)
+        :param kappa: the kappa constriction coefficient. It should
+        be in [0, 1]
+        :param phi1: the phi1 constriction coefficient. phi1 + phi2
+        must be greater than or equal to 2
+        :param phi2: the phi2 constriction coefficient. phi1 + phi2
+        must be greater than or equal to 2
+        :param numIterations: number of iterations to be performed
+        :return: (optimized parameters,
+            optimal value of the function,
+            global best cost values at each iteration),
+        where the optimized parameters is a numpy array of shape (dim,)
+        and optimal value is the value of the function achieved by these
+        parameters
         """
 
         phi = phi1 + phi2
