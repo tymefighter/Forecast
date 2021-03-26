c4666f1ec8a1226ea97fc721248d49ef5d6d6a10
Ahmed Zaheer Dadarkar <ahmed.dadarkar@gmail.com>
2021-03-26 18:43:29 +0530

    Add Model

--- ts/data/dataset/__init__.py
+++ ts/data/dataset/__init__.py
@@ -3,3 +3,4 @@ from ts.data.dataset.retail_sales import RetailSales
 from ts.data.dataset.jaipur_weather import JaipurWeather
 from ts.data.dataset.indian_subdivision_rainfall import IndianSubdivisionRainfall
 from ts.data.dataset.nifty_fifty_stock import NiftyFiftyStock
+from ts.data.dataset.perth_temp_and_rainfall import PerthTempAndRainfall
